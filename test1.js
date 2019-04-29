beebc2c40100f7bb1134907b52f6292ff3842c06
Matt Kunnari <mkunnari1@gmail.com>
2019-04-28 21:30:51 -0700

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,6 +1,5 @@
-function convertToCelsius(tempInF) {
-    tempInC = (tempInF-32) * 5/9;
-    return tempInC
+for (let i=0; i <= 10000; i++) {
+    if((i % 2) == 0)
+    console.log(i);
 }
-
-console.log( convertToCelsius(212) );
+   
